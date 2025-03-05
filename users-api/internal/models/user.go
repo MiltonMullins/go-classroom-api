cf41eca4b90d5684119751b20a04a831eab38fd8
MiltonMullins <miltonmullins92@gmail.com>
2025-03-05 00:08:49 -0300

    implement user model

--- users-api/internal/models/user.go
+++ users-api/internal/models/user.go
@@ -5,6 +5,15 @@ type User struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
-	Role 	string `json:"role"`
+	Role      string `json:"role"`
 }
 
+func NewUser(id int, firstName, lastName, email, role string) *User {
+	return &User{
+		ID:        id,
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Role:      role,
+	}
+}
