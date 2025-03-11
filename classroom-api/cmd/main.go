fe81565facb86340861bbd57534b0f39c29d8f6d
MiltonMullins <miltonmullins92@gmail.com>
2025-03-11 11:08:03 -0300

    add database creation and routes for classroom service

--- classroom-api/cmd/main.go
+++ classroom-api/cmd/main.go
@@ -21,11 +21,22 @@ func main() {
 	}
 	defer db.Close()
 
+	//create the table classrooms if not exists
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS classrooms (
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		description VARCHAR(1000) NOT NULL,
+		teacher_id int NOT NULL
+	);`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	//create the table participants if not exists
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS participants (
 		id SERIAL PRIMARY KEY,
 		classroom_id int references classrooms(id),
-		user_id int references users(id),
+		user_id int NOT NULL,
 		role VARCHAR(100) NOT NULL
 	);`)
 	if err != nil {
@@ -35,10 +46,10 @@ func main() {
 	router := initializeRoutes(db) // configure routes
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":8081",
 		Handler: router,
 	}
-	log.Println("Listening on port:8080...")
+	log.Println("Listening on port:8081...")
 	server.ListenAndServe() // Run the http server
 }
 
@@ -47,9 +58,23 @@ func initializeRoutes(db *sql.DB) *http.ServeMux {
 		services.NewParticipantsService(
 			repositories.NewParticipantsRepository(db)))
 
+	classroomsHandler := handlers.NewClassroomHandler(
+		services.NewClassroomService(
+			repositories.NewClassroomRepository(db)))
+
 	mux := http.NewServeMux()
-	mux.Handle("GET /participants", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.GetParticipantsByClassroomID))))
-	mux.Handle("POST /participants", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.CreateParticipant))))
-	mux.Handle("DELETE /participants", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.DeleteParticipant))))
+
+	//Participants routes
+	mux.Handle("GET /participants/{classroomID}", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.GetParticipantsByClassroomID))))
+	mux.Handle("POST /participant", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.CreateParticipant))))
+	mux.Handle("DELETE /participant/{classroomID}/{id}", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(participantsHandler.DeleteParticipant))))
+
+	//Classrooms routes
+	mux.Handle("GET /classrooms", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(classroomsHandler.GetClassrooms))))
+	mux.Handle("GET /classroom/{id}", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(classroomsHandler.GetClassroomByID))))
+	mux.Handle("POST /classrooms", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(classroomsHandler.CreateClassroom))))
+	mux.Handle("PUT /classroom/{id}", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(classroomsHandler.UpdateClassroom))))
+	mux.Handle("DELETE /classroom/{id}", middleware.JwtAuthMiddleware(middleware.Log(http.HandlerFunc(classroomsHandler.DeleteClassroom))))
+
 	return mux
 }
