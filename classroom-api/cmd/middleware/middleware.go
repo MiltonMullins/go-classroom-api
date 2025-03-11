90795021dead162207ddc51a981fec7c95ba19ed
MiltonMullins <miltonmullins92@gmail.com>
2025-03-11 08:50:48 -0300

    copy middleware and jwt auth

---
+++ classroom-api/cmd/middleware/middleware.go
@@ -0,0 +1,57 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/miltonmullins/classroom-api/classroom-api/utils"
+)
+
+func JwtAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Executing JWT Auth")
+		//w.Header().Set("Content-Type", "application/json") necesario?
+		
+		authHeader := r.Header.Get("Authorization")
+		tokenSplit := strings.Split(authHeader, " ")
+		if len(tokenSplit) != 2 {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Invalid authorization header")
+			return
+		}
+
+		authToken := tokenSplit[1]
+
+		err := jwt.VerifyToken(authToken)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Invalid token")
+			return
+		}
+
+		email, err := jwt.ExtractIDFromToken(authToken)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Fprint(w, "Invalid token")
+			return
+		}
+		w.Header().Set("x-user-email", email)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func Log(next http.Handler) http.Handler {
+	log.Printf("Log: %v", time.Now())
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Executing middlewareLog")
+		if r.URL.Path == "/foo" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+		log.Print("Executing middlewareLog again")
+	})
+}
